e61849442751f6277825e654025d63bbb54b9e49
luxiaolong <luxiaolong02@meituan.com>
2018-02-13 14:09:29 +0800

    init

---
+++ src/pages/index/boot-loader.jsx
@@ -0,0 +1,6 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import Index    from './index';
+import '@utils/reset-app.less';
+
+ReactDOM.render(<Index />, document.getElementById('app-root'));
---
+++ src/pages/index/context/index.js
@@ -0,0 +1,24 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+
+// todoList: [{content, isFinished}, ...rest]
+const TodoContext = React.createContext({
+    todoList: [],
+    addTodoItem: () => {},
+    delTodoItem: () => {},
+});
+
+export const Provider = TodoContext.Provider;
+export const Consumer = TodoContext.Consumer;
+
+export const connect = mapStateToProps => Component => class extends React.Component {
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <Consumer>{state => <Component {...mapStateToProps(state)} />}</Consumer>
+        );
+    }
+}
